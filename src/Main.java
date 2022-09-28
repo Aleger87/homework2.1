350cf34781a5c9f0894495fe58029861d6318ab4
Aleksandr Gerb <aleksandrgerb87@gmail.com>
2022-09-29 00:37:04 +0300

    hw2.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Person person = new Person();
+    person.name = "Lyapis";
+    person.surname = "Trubetskoy";
+    changeValue(person);
+    System.out.println(person.toString());
+    }
+
+    private static void changeValue(Person person) {
+        //person = new Person();
+        person.name = "Ilya";
+        person.surname = "Lagutenko";
+
+    }
+}
+
+class Person {
+    String name;
+    String surname;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
     }
 }
